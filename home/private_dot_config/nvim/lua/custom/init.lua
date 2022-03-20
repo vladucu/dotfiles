17ce394dca417a61f6ace5520e936bf139b93665
Vlad Vanca <vanca.vlad@gmail.com>
2022-03-20 16:52:06 +0200

    Add another neovim reference

--- home/private_dot_config/nvim/lua/custom/init.lua
+++ home/private_dot_config/nvim/lua/custom/init.lua
@@ -1,5 +1,6 @@
 -- Sources
 -- https://github.com/nanotee/nvim-lua-guide
+-- https://github.com/rockerBOO/awesome-neovim
 -- https://github.com/siduck/dotfiles/tree/master/nvchad/custom
 
 require("custom.mappings")
