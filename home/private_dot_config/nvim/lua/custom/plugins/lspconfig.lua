28c8f4d51513264f02ddbd852fc6f0c309faa6f6
Vlad Vanca <vanca.vlad@gmail.com>
2022-04-10 22:48:38 +0300

    Try new neovim

--- home/private_dot_config/nvim/lua/custom/plugins/lspconfig.lua
+++ home/private_dot_config/nvim/lua/custom/plugins/lspconfig.lua
@@ -1,20 +1,29 @@
 local M = {}
 
 M.setup_lsp = function(attach, capabilities)
-   local lspconfig = require "lspconfig"
+  local lspconfig = require "lspconfig"
 
-   -- lspservers with default config
-   local servers = { "html", "cssls", "ember" }
+  -- lspservers with default config
+  local servers = { "html", "cssls", "ember"}
 
-   for _, lsp in ipairs(servers) do
-      lspconfig[lsp].setup {
-         on_attach = attach,
-         capabilities = capabilities,
-         flags = {
-            debounce_text_changes = 150,
-         },
-      }
-   end
+  for _, lsp in ipairs(servers) do
+    lspconfig[lsp].setup {
+       on_attach = attach,
+       capabilities = capabilities,
+       flags = {
+          debounce_text_changes = 150,
+       },
+    }
+  end
+
+  lspconfig.elixirls.setup {
+    on_attach = attach,
+    capabilities = capabilities,
+    cmd = { "/Users/vladucu/code/github.com/elixir-lsp/elixir-ls/rel/language_server.sh" },
+    flags = {
+      debounce_text_changes = 150,
+    },
+  }
 end
 
 return M
