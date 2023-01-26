7d9a217c83a2c1ba3d3ef77a5712708e8ec67e41
Vlad Vanca <vanca.vlad@gmail.com>
2023-01-26 21:56:34 +0200

    Add zen-mode

--- home/private_dot_config/astronvim/lua/user/plugins/init.lua
+++ home/private_dot_config/astronvim/lua/user/plugins/init.lua
@@ -5,6 +5,10 @@ return {
       require("nightfox").setup(require "user.plugins.nightfox")
     end,
   },
+  ["folke/zen-mode.nvim"] = {
+    cmd = { "ZenMode" },
+    config = function() require("user.plugins.zen-mode").config() end,
+  },
   ["wakatime/vim-wakatime"] = {
     -- opt = true,
     -- setup = function() table.insert(astronvim.file_plugins, "vim-wakatime") end,
