75d6de1f6a9d830bc3c3dceeb424a340d17ba834
Vlad Vanca <vanca.vlad@gmail.com>
2023-02-25 13:24:38 +0200

    Remove old nvim config

--- home/private_dot_config/nvim2/lua/custom/init.lua
+++
@@ -1,9 +0,0 @@
--- Sources
--- https://github.com/nanotee/nvim-lua-guide
--- https://github.com/rockerBOO/awesome-neovim
--- https://github.com/siduck/dotfiles/tree/master/nvchad/custom
-
--- GUI
-vim.api.nvim_exec([[set guifont=Hack\ Nerd\ Font:h13]], false)
-
-require("custom.mappings")
