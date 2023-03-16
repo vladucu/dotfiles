6127a5d12d41707e92b5bf801e85be83f4eaa32d
Vlad Vanca <vanca.vlad@gmail.com>
2023-03-16 22:54:27 +0200

    More plugins

--- home/private_dot_config/astronvim/lua/user/plugins/community.lua
+++ home/private_dot_config/astronvim/lua/user/plugins/community.lua
@@ -4,13 +4,19 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.kanagawa" },
+  { import = "astrocommunity.colorscheme.oxocarbon" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.editing-support.neogen" },
+  { import = "astrocommunity.editing-support.treej" },
   { import = "astrocommunity.editing-support.zen-mode-nvim" },
   { import = "astrocommunity.media.vim-wakatime" },
+  { import = "astrocommunity.motion.mini-ai" },
+  { import = "astrocommunity.motion.mini-basics" },
   { import = "astrocommunity.motion.mini-move" },
+  { import = "astrocommunity.motion.mini-bracketed" },
+  { import = "astrocommunity.motion.mini-surround" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.json" },
@@ -19,4 +25,5 @@ return {
   { import = "astrocommunity.pack.nix" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.project.nvim-spectre" },
 }
