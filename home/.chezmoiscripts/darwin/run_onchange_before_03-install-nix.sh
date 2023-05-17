16953b134b28c1b70cb1cc47af69af6ad8d534ae
Vlad Vanca <vanca.vlad@gmail.com>
2023-05-17 10:52:40 +0300

    Tweak nix-installer

--- home/.chezmoiscripts/darwin/run_onchange_before_03-install-nix.sh
+++ home/.chezmoiscripts/darwin/run_onchange_before_03-install-nix.sh
@@ -5,7 +5,13 @@
 source /tmp/chezmoi-utils.sh
 set -eufo pipefail
 
-printf "${green}\n[03 - macOS] ${reset}"
-echo "Installing nix"
+if command -v nix >/dev/null; then
+  echo "Homebrew already installed, skipping."
+  exit 0
+else
+  printf "${green}\n[03 - macOS] ${reset}"
+  echo "Installing nix"
+fi
 
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
